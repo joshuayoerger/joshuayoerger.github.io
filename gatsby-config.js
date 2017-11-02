bbc861ce9ed273f129373546200adc754393b087
Joshua Yoerger <joshua.yoerger@gmail.com>
2017-11-01 21:58:24 -0700

    Use React 16
    Update eslint version and as well as experiment with react-feather package

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-react-next`,
     `gatsby-plugin-glamor`,
     {
       resolve: `gatsby-plugin-typography`,
